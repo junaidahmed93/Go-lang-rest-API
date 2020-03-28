34ce7b2e96380177e179f980242c270653bf35f9
VD-JUNAID\Junaid <junaid.ahmed@venturedive.com>
2020-03-28 16:22:34 +0500

    POST API with gollira mux

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
@@ -18,19 +19,25 @@ type Articles []Article
 func allArticles(w http.ResponseWriter, r *http.Request) {
 	articles := Articles {
 		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
-
 	}
 	fmt.Println("Endpoint Hit: All Articles Endpoint")
 	json.NewEncoder(w).Encode(articles)
+}
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Post API")
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint hit")
 }
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles);
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main(){
