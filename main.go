252521ac4a171c8eb02f6194011e74cd6391c69b
VD-JUNAID\Junaid <junaid.ahmed@venturedive.com>
2020-03-28 14:50:44 +0500

    Basic server setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint hit")
+}
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main(){
+	handleRequests()
+}
