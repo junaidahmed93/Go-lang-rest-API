75cd2535c5c783e6885df9c8e81b3b3e3093bbce
VD-JUNAID\Junaid <junaid.ahmed@venturedive.com>
2020-05-09 23:49:51 +0500

    Test with Auth routes

--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
 )
 
 type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
 type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles {
+	articles := Articles{
 		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
 	}
 	fmt.Println("Endpoint Hit: All Articles Endpoint")
@@ -25,7 +27,11 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 }
 
 func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Post API")
+	// pathParams := mux.Vars(r)
+	// w.Header().Set("Content-Type", "application/json")
+	// fmt.Println("pathParams")
+	fmt.Fprintf(w, "POST")
+	// fmt.Fprintf(w, pathParams)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +46,38 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
-func main(){
-	handleRequests()
+func main() {
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	// Per route middleware, you can add as many as you desire.
+	r.GET("/benchmark", MyBenchLogger(), benchEndpoint)
+
+	// Authorization group
+	// authorized := r.Group("/", AuthRequired())
+	// exactly the same as:
+	authorized := r.Group("/")
+	// per group middleware! in this case we use the custom created
+	// AuthRequired() middleware just in the "authorized" group.
+	authorized.Use(AuthRequired())
+	{
+		authorized.POST("/login", loginEndpoint)
+		authorized.POST("/submit", submitEndpoint)
+		authorized.POST("/read", readEndpoint)
+
+		// nested group
+		testing := authorized.Group("testing")
+		testing.GET("/analytics", analyticsEndpoint)
+	}
+
+	// Listen and serve on 0.0.0.0:8080
+	r.Run(":8080")
 }
