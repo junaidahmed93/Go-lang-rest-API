0e9aae112678baa13f12eec398c7b382f6f54f7c
VD-JUNAID\Junaid <junaid.ahmed@venturedive.com>
2020-05-29 16:57:13 +0500

    Sign In and Sign Up

--- hotel.go
+++ hotel.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -8,18 +9,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *gorm.DB
 
 func init() {
 	var err error
-	db, err = gorm.Open("mysql", "<DB-USER>:<DB-PASSWORD>@/<DB-NAME>?charset=utf8&parseTime=True&loc=Local")
+	db, err = gorm.Open("mysql", "root:karachi123@/hotel-management?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("Unable to connect to database")
 	}
 
-	db.AutoMigrate(&hotel{}, &room{})
+	db.AutoMigrate(&hotel{}, &room{}, &user{})
 }
 
 func main() {
@@ -27,7 +29,9 @@ func main() {
 	router := gin.Default()
 
 	v1 := router.Group("/api")
-	{
+	{	
+		v1.POST("/signup", signUp)
+		v1.POST("/signin", signIn)
 		v1.POST("/hotel", createHotel)
 		v1.POST("/room", createRoom)
 		v1.GET("/available-room", availableRoom)
@@ -37,6 +41,13 @@ func main() {
 }
 
 type (
+	user struct {
+		gorm.Model
+		Email string `json:"email"`
+		Password   string    `gorm:"size:100;not null;" json:"password"`
+		// Password []byte `json:"password"`
+	}
+
 	hotel struct {
 		gorm.Model
 		Name string `json:"name"`
@@ -54,6 +65,51 @@ type (
 		BookedBy  int
 	}
 )
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func signUp(c *gin.Context) {
+	email := c.PostForm("email")
+	hashedPassword, err := Hash(c.PostForm("password"))
+	passwordAsString := string(hashedPassword)
+	if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError, "message": "Bycrypt error",})
+	}	
+    var admin []user 
+	db.Where("email = ?", email).Find(&admin)
+	// fmt.Printf("%+v\n", admin)
+	// if len(*admin) {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": "Email already exist",})
+	// }
+	user := user{Email: email, Password: passwordAsString}
+	db.Save(&user)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "User created successfully", "email": user.Email})
+}
+
+func signIn(c *gin.Context) {
+	email := c.PostForm("email")
+	enteredPassword := c.PostForm("password")
+	
+    admin :=user{}
+	db.Where("email = ?", email).Find(&admin)
+
+	err := VerifyPassword(admin.Password, enteredPassword)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": http.StatusUnauthorized, "message": "Wrong credentials"})
+		return
+	}
+	fmt.Printf("%+v\n", admin)
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Logged In"})
+	
+}
+
+
 
 func createHotel(c *gin.Context) {
 	hotel := hotel{Name: c.PostForm("name"), Area: c.PostForm("area")}
